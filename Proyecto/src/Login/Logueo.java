cecf6f89269b2d2128f12b45cfb34132152da685
urquijonotbat <55584487+urquijonotbat@users.noreply.github.com>
2019-10-29 14:09:59 +0100

    añadida parte de miguel

---
+++ Proyecto/src/Login/Logueo.java
@@ -0,0 +1,108 @@
+package Login;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Logueo extends JFrame {
+
+	private JPanel btnLoguear;
+	private JPasswordField jpassClave;
+	private JTextField textUser;
+	private JButton btnLogin;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Logueo frame = new Logueo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Logueo() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(430, 300);
+		setResizable(false);
+		setLocationRelativeTo (null);
+		btnLoguear = new JPanel();
+		btnLoguear.setBackground(new Color(192, 192, 192));
+		btnLoguear.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(btnLoguear);
+		btnLoguear.setLayout(null);
+		
+		JLabel lblLogin = new JLabel("LOGIN");
+		lblLogin.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblLogin.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLogin.setBounds(114, 24, 189, 22);
+		btnLoguear.add(lblLogin);
+		
+		JLabel lblUser = new JLabel("User :");
+		lblUser.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblUser.setBounds(55, 76, 56, 16);
+		btnLoguear.add(lblUser);
+		
+		JLabel lblPassword = new JLabel("Password :");
+		lblPassword.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblPassword.setBounds(55, 119, 97, 16);
+		btnLoguear.add(lblPassword);
+		
+		jpassClave = new JPasswordField();
+		jpassClave.setBounds(185, 116, 169, 22);
+		btnLoguear.add(jpassClave);
+		
+		textUser = new JTextField();
+		textUser.setBounds(185, 70, 169, 22);
+		btnLoguear.add(textUser);
+		textUser.setColumns(10);
+		
+		btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				char [] contraseña = jpassClave.getPassword();
+				String contraseñaFinal = new String (contraseña);
+				String user = textUser.getText();
+				
+				if (textUser.getText().equals("Miguel") && contraseñaFinal.contentEquals("12345")) {
+					dispose();
+					Main p = new Main();
+					p.setVisible(true);
+				}else {
+					JOptionPane.showMessageDialog(null, "Usuario o Contraseña incorrectos", "ERROR",
+							JOptionPane.ERROR_MESSAGE);
+					textUser.setText("");
+					jpassClave.setText("");
+					textUser.requestFocus();
+				}
+				
+			}
+		});
+		btnLogin.setBounds(168, 180, 97, 25);
+		btnLoguear.add(btnLogin);
+	}
+}
---
+++ Proyecto/src/Login/Usuario.java
@@ -0,0 +1,60 @@
+	package Login;
+
+	public class Usuario {
+    
+	private int idUsuario;
+	private String nombre_usuario;
+    private int tipo_usuario;
+    private String contrasenya_usuario;
+    private String correo_usuario;
+    
+    public Usuario (){
+    	super();
+    }
+    public Usuario(int idUsuario, String nombre_usuario, int tipo_usuario, String contrasenya_usuario) {
+    	super();
+    	this.idUsuario = idUsuario;
+    	this.nombre_usuario = nombre_usuario;
+    	this.tipo_usuario = tipo_usuario;
+    	this.contrasenya_usuario = contrasenya_usuario;
+    	this.correo_usuario = correo_usuario;
+    	
+    }
+    public int getIdUsuario() {
+    	return idUsuario;
+    }
+    public void setIdUsuario(int idUsuario) {
+    	this.idUsuario = idUsuario;
+    }
+	
+    public String getnombre_usuario() {
+		return nombre_usuario;
+	}
+	public void setnombre_usuario(String nombre_usuario) {
+		this.nombre_usuario = nombre_usuario;
+	}
+	
+	public int gettipo_usuario() {
+		return tipo_usuario;
+	}
+	public void settipo_usuario(int tipo_usuario) {
+		this.tipo_usuario = tipo_usuario;
+	}
+
+	public String getcontrasenya_usuario() {
+		return contrasenya_usuario;
+	}
+	public void setcontrasenya_usuario(String contrasenya_usuario) {
+		this.contrasenya_usuario = contrasenya_usuario;
+	}
+    
+	public String getCorreo_usuario() {
+		return correo_usuario;
+	}
+	public void setCorreo_usuario(String correo_usuario) {
+		this.correo_usuario = correo_usuario;
+	}
+	
+	
+	
+}
