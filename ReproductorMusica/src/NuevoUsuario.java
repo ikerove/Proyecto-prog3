be7e2344af070c210a58526d70ca33f097904a1e
ikerove <52752754+ikerove@users.noreply.github.com>
2019-12-01 13:45:21 +0100

    Creada clase nuevo usuario
    -Añadido funcion boton registrarse

--- ReproductorMusica/src/NuevoUsuario.java
+++ ReproductorMusica/src/NuevoUsuario.java
@@ -4,10 +4,25 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class NuevoUsuario extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JLabel lblNewLabel_2;
+	private JTextField textField_2;
+	private JLabel lblNewLabel_3;
+	private JTextField textField_3;
+	private JLabel lblNewLabel_4;
+	private JButton btnNewButton;
 
 	/**
 	 * Launch the application.
@@ -35,6 +50,60 @@ public class NuevoUsuario extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(new GridLayout(5, 2, 0, 0));
+		
+		JLabel lblNewLabel = new JLabel("Correo electronico");
+		panel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Usuario");
+		panel.add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		lblNewLabel_2 = new JLabel("Contrasena");
+		panel.add(lblNewLabel_2);
+		
+		textField_2 = new JTextField();
+		panel.add(textField_2);
+		textField_2.setColumns(10);
+		
+		lblNewLabel_3 = new JLabel("Contrasena");
+		panel.add(lblNewLabel_3);
+		
+		textField_3 = new JTextField();
+		panel.add(textField_3);
+		textField_3.setColumns(10);
+		
+		lblNewLabel_4 = new JLabel("Bienvenido a");
+		contentPane.add(lblNewLabel_4, BorderLayout.NORTH);
+		
+		btnNewButton = new JButton("Registrarse");
+		contentPane.add(btnNewButton, BorderLayout.SOUTH);
+		btnNewButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				if(textField_2.getText().equals(textField_3.getText())) {
+					System.out.println("CORRECTO");
+				}
+				else {
+					System.out.println("INCORRECTO");
+				}
+			}
+		});
+		
+		
+		
 	}
 
 }
