0e87d8cd184a41f1313dbf298a939f8e8c382deb
ikerove <52752754+ikerove@users.noreply.github.com>
2019-12-27 14:14:12 +0100

    modificaciones y metodos

--- ReproductorMusica/src/VentanaPrincipal.java
+++ ReproductorMusica/src/VentanaPrincipal.java
@@ -57,6 +57,9 @@ import org.jaudiotagger.tag.TagException;
 
 public class VentanaPrincipal extends JFrame {
  //prueba
+	
+	basic_playerlistener();
+	
 	private JPanel contentPane;
 	private JFrame frame;
 	private JButton btnPlaypausa = new JButton("Play/Pausa");
@@ -151,7 +154,7 @@ public VentanaPrincipal() {
 		
 		setBounds(100, 100, 573, 329);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(700, 500);
+		setSize(700, 501);
 		setLocationRelativeTo (null);
 		
 		setJMenuBar(menuBar);
@@ -174,14 +177,14 @@ public VentanaPrincipal() {
 		getContentPane().setLayout(null);
 		panel_1.setBounds(0, 0, 53, 373);
 		panel_1.setLayout(null);
-		p1Arriba.setBounds(5, 5, 43, 181);
+		p1Arriba.setBounds(5, 5, 65, 181);
 		
 		panel_1.add(p1Arriba);
-		p1Abajo.setBounds(5, 186, 43, 181);
+		p1Abajo.setBounds(5, 186, 65, 181);
 		panel_1.add(p1Abajo);
 		p1Abajo.setLayout(null);
 		p1Arriba.setLayout(null);
-		lblUnidad.setBounds(5, 5, 33, 14);
+		lblUnidad.setBounds(5, 5, 54, 14);
 		
 		
 		p1Arriba.add(lblUnidad);
@@ -193,11 +196,13 @@ public VentanaPrincipal() {
 		
 		
 		panel_2.add(p2Arriba);
-		p2Abajo.setBounds(0, 186, 577, 186);
+		p2Abajo.setBounds(0, 181, 577, 186);
 		panel_2.add(p2Abajo);
 		p2Abajo.setLayout(null);
+		listaCanciones.setBounds(0, 177, 573, -302);
+		p2Abajo.add(listaCanciones);
 		p2Arriba.setLayout(null);
-		lblPlaylist.setBounds(269, 5, 33, 14);
+		lblPlaylist.setBounds(48, 6, 264, 22);
 		
 		
 		
@@ -205,13 +210,10 @@ public VentanaPrincipal() {
 		
 		
 		p2Arriba.add(lblPlaylist);
-		listaCanciones.setBounds(307, 12, 0, 0);
-		//panel_2.setBorder(new EmptyBorder(5, 5, 5, 200));
-		p2Arriba.add(listaCanciones);
 		getContentPane().add(panel_2);
 		panel_3.setBounds(630, 0, 54, 373);
 		panel_3.setLayout(null);
-		p3Arriba.setBounds(5, 5, 44, 181);
+		p3Arriba.setBounds(5, 6, 64, 180);
 		
 		
 		
@@ -220,7 +222,7 @@ public VentanaPrincipal() {
 		panel_3.add(p3Abajo);
 		p3Abajo.setLayout(null);
 		p3Arriba.setLayout(null);
-		lblAmigos.setBounds(5, 5, 34, 14);
+		lblAmigos.setBounds(5, 5, 53, 14);
 		
 		p3Arriba.add(lblAmigos);
 		panel_3.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -231,25 +233,25 @@ public VentanaPrincipal() {
 		
 		panel_4.add(p4Arriba);
 		p4Arriba.setLayout(null);
-		lblCaratula.setBounds(203, 9, 41, 14);
+		lblCaratula.setBounds(113, 9, 92, 14);
 		
 		p4Arriba.add(lblCaratula);
-		btnAtras.setBounds(249, 5, 59, 23);
+		btnAtras.setBounds(205, 6, 59, 23);
 		
 		p4Arriba.add(btnAtras);
-		btnPlaypausa.setBounds(313, 5, 85, 23);
+		btnPlaypausa.setBounds(291, 5, 107, 23);
 	//	btnPlaypausa.setIcon(new ImageIcon("/Users/ikerrodriguez/Downloads/Unknown"));
 		p4Arriba.add(btnPlaypausa);
-		btnSiguiente.setBounds(403, 5, 77, 23);
+		btnSiguiente.setBounds(403, 5, 120, 23);
 		
 		p4Arriba.add(btnSiguiente);
-		p4Abajo.setBounds(0, 33, 684, 33);
+		p4Abajo.setBounds(0, 33, 684, 51);
 		panel_4.add(p4Abajo);
 		p4Abajo.setLayout(null);
-		lblCancion.setBounds(247, 5, 38, 14);
+		lblCancion.setBounds(6, 17, 269, 14);
 		lblCancion.setHorizontalAlignment(SwingConstants.CENTER);
 		p4Abajo.add(lblCancion);
-		pb.setBounds(290, 5, 146, 14);
+		pb.setBounds(287, 17, 232, 14);
 		p4Abajo.add(pb);
 		getContentPane().add(panel_4);
 		
@@ -298,9 +300,73 @@ public VentanaPrincipal() {
 			}
 		});
 		
+		
+		public void basic_playerlistener() {
+			Audio.addBasicPlayerListener(new BasicPlayerListener() {
+				
+				@Override
+				public void stateUpdated(BasicPlayerEvent bpe) {
+					// TODO Auto-generated method stub
+					if (!bloquear){
+	                    if (Audio.getStatus()==2 & repitaCancion){
+	                        jButtonReproducir.doClick();
+	                    }
+	                    if (jListListaCanciones.getSelectedIndex()+1!=agregaCanciones.length){
+	                        if (Audio.getStatus()==2 & siguiente){
+	                            int pista = jListListaCanciones.getAnchorSelectionIndex();                            
+	                            jListListaCanciones.setSelectedIndex(pista+1);
+	                            repaint();
+	                            jButtonReproducir.doClick();
+	                        }
+	                    }
+	                }                
+	            
+				}
+				
+				@Override
+				public void setController(BasicController arg0) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void progress(int i, long l, byte[] bytes, Map propiedades) {
+					// TODO Auto-generated method stub
+					 CalculoSecundero(propiedades.get("mp3.position.microseconds").toString(), "Transcurrido: ", jLabelTranscurrido);
+
+		                Object bytesTranscurrido =  propiedades.get("mp3.position.byte");
+		                bytesTranscurrido= Integer.parseInt(bytesTranscurrido.toString());               
+		                jSliderProgresoMp3.setValue((int)bytesTranscurrido);
+				}
+				
+				@Override
+				public void opened(Object o, Map map) {
+					// TODO Auto-generated method stub
+					 CalculoSecundero(map.get("duration").toString(), "Duracion: ", jLabelTiempo);
+		               
+		               new JLaTexto(fuente1, "Tasa de bits: "+map.get("bitrate"), jLabelBitrate, c, 15);
+		               new JLaTexto(fuente1, "Velocidad Muestreo: "+map.get("mp3.frequency.hz"), jLabelFRate, c, 15);
+
+		               jSliderProgresoMp3.setMaximum(Integer.parseInt(map.get("mp3.length.bytes").toString()));
+		               jSliderProgresoMp3.setMinimum(0);
+				}
+			});
+			
+		}
+		
+		
+		 
+		
+		
+		
+		
+		
+		
 		}
 
 		}
+
+	
 		
 		//btnPlaypausa.setBounds(168,183,97,22);
 		
