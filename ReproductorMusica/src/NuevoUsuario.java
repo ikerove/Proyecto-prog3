fd69c3edc9107e2bc0a82028d364faf60a85e9ef
urquijonotbat <miguelurquijofabeiro@opendeusto.es>
2019-12-27 14:31:41 +0100

    New user
    modificaciones en la ventana

--- ReproductorMusica/src/NuevoUsuario.java
+++ ReproductorMusica/src/NuevoUsuario.java
@@ -54,46 +54,57 @@ public class NuevoUsuario extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
 		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.CENTER);
-		panel.setLayout(new GridLayout(5, 2, 0, 0));
+		panel.setBounds(5, 19, 424, 181);
+		contentPane.add(panel);
+		panel.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Correo electronico");
+		JLabel lblNewLabel = new JLabel("Correo electronico:");
+		lblNewLabel.setBounds(31, 10, 122, 27);
 		panel.add(lblNewLabel);
 		
 		textField = new JTextField();
+		textField.setBounds(163, 10, 226, 31);
 		panel.add(textField);
 		textField.setColumns(10);
 		
-		JLabel lblNewLabel_1 = new JLabel("Usuario");
+		JLabel lblNewLabel_1 = new JLabel("Usuario:");
+		lblNewLabel_1.setBounds(31, 59, 122, 27);
 		panel.add(lblNewLabel_1);
 		
 		textField_1 = new JTextField();
+		textField_1.setBounds(163, 55, 134, 31);
 		panel.add(textField_1);
 		textField_1.setColumns(10);
 		
-		lblNewLabel_2 = new JLabel("Contrasena");
+		lblNewLabel_2 = new JLabel("Contrasena:");
+		lblNewLabel_2.setBounds(31, 101, 122, 27);
 		panel.add(lblNewLabel_2);
 		
 		textField_2 = new JTextField();
+		textField_2.setBounds(163, 99, 200, 31);
 		panel.add(textField_2);
 		textField_2.setColumns(10);
 		
-		lblNewLabel_3 = new JLabel("Contrasena");
+		lblNewLabel_3 = new JLabel("Repita su Contrasena:");
+		lblNewLabel_3.setBounds(31, 143, 122, 27);
 		panel.add(lblNewLabel_3);
 		
 		textField_3 = new JTextField();
+		textField_3.setBounds(163, 142, 200, 29);
 		panel.add(textField_3);
 		textField_3.setColumns(10);
 		
 		lblNewLabel_4 = new JLabel("Bienvenido a");
-		contentPane.add(lblNewLabel_4, BorderLayout.NORTH);
+		lblNewLabel_4.setBounds(5, 5, 424, 14);
+		contentPane.add(lblNewLabel_4);
 		
 		btnNewButton = new JButton("Registrarse");
-		contentPane.add(btnNewButton, BorderLayout.SOUTH);
+		btnNewButton.setBounds(5, 211, 424, 45);
+		contentPane.add(btnNewButton);
 		btnNewButton.addActionListener(new ActionListener() {
 			
 			@Override
