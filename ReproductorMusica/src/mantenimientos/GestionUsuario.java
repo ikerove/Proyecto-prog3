76f03b01b23cb615309f99d5af0dfb8cabcdd084
urquijonotbat <miguelurquijofabeiro@opendeusto.es>
2020-01-02 16:56:41 +0100

    cambios varios

--- ReproductorMusica/src/mantenimientos/GestionUsuario.java
+++ ReproductorMusica/src/mantenimientos/GestionUsuario.java
@@ -3,11 +3,46 @@ package mantenimientos;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import ConectorBD.MySQLConexion;
 import mantenimientos.Usuario;
 
 public class GestionUsuario {
+	
+	public Usuario registrarUsuario(Usuario usu) {
+		
+		Usuario usuario=null;
+		
+		Connection con=null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		
+			con = MySQLConexion.getConexion();
+			
+			String sql = "INSERT INTO Usuario (id_usuario, nombre_usuario, tipo_usuario, contrasenya_usuario, correo_usuario) values (?,?,?,?)";
+			
+			
+			try {
+				pst = con.prepareStatement(sql);
+				pst.setInt(1, usu.getIdUsuario());
+				pst.setString(2, usu.getNombre_usuario());
+				pst.setString(3, usu.getContrasenya_usuario());
+				pst.setString(4, usu.getCorreo_usuario());
+				pst.execute();
+				return usuario;
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return usuario;
+			
+			
+			
+			
+		
+	}
 	
 	public Usuario obtenerUsuario(Usuario usu) {
 		
